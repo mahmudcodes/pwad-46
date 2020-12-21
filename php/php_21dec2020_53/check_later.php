5f5d1b93248832f5b701d59fab5f3242c122463d
next-mahmud <54480128+next-mahmud@users.noreply.github.com>
2020-12-21 13:11:04 +0600

    js/php_21_dec

---
+++ php/php_21dec2020_53/check_later.php
@@ -0,0 +1,41 @@
+<h2>Do not get this. Understand later...</h2>
+<?php 
+	$i=0;
+	while ($i<=10) {
+		// if($i<=5) {
+		// 	break;
+		// }
+		print $i;
+		$i=$i+1;
+	}
+?>
+<hr>
+<?php 
+	$i=0;
+	while ($i<=10) {
+		print $i;
+		$i=$i+2;
+	}
+?>
+<hr>
+<?php 
+	$i=0;
+	while ($i<=10) {
+		// if($i<=5) {
+		// 	break;
+		// }
+		print $i;
+		$i=$i+2;
+	}
+?>
+<hr>
+<?php 
+	$i=0;
+	while ($i<=10) {
+		// if($i<=5) {
+		// 	continue;
+		// }
+		print $i;
+		$i=$i+2;
+	}
+?>
---
+++ php/php_21dec2020_53/test.php
@@ -0,0 +1,6 @@
+<?php
+
+	$$str= "bye";
+
+	echo $$str;
+?>
